f9cfb329a7be21d2b2ee5137512dd341be323369
ndodanli <ndodanli14@gmail.com>
2022-10-10 23:50:50 +0300

    merge

--- mytests/mytests.go
+++ mytests/mytests.go
@@ -2,60 +2,24 @@ package main
 
 import (
 	"fmt"
-	"strings"
+	"time"
 )
 
-type Shape interface {
-	Area() float64
-	Test() string
-}
-
-type Skin interface {
-	Color() float64
-}
-
-type Cube struct {
-	side   float64
-	radius float64
-}
-
-func (c Cube) Area() float64 {
-	fmt.Printf("memory address of c is %p\n", &c)
-	return c.side * c.side * c.side * c.radius
-}
-
-func (c *Cube) Test() string {
-	fmt.Printf("memory address of *c is %p\n", c)
-
-	return "test"
-}
-
 func main() {
-	rr := Cube{side: 2, radius: 3}
-	var r Shape = &rr
-	fmt.Printf("memory address of r is %p\n", &r)
-	r.Area()
-	r.Test()
+	start := time.Now()
+	arr := [100000]int{}
 
-	// var s Shape = &r
-	// s.Area()
-	// s.Test()
-	// explain(s, "s")
-
-	// v, ok := s.(Cube)
-	// fmt.Println("v", "ok:", ok)
-	// explain(v, "v")
-
-}
-
-func explain(v interface{}, key string) {
-	fmt.Printf("%v value is %v\n", key, v)
-	fmt.Printf("%v type is %T\n", key, v)
-	switch v.(type) {
-	case string:
-		fmt.Printf("%v in uppercase if string", strings.ToUpper(v.(string)))
-	case Cube:
-		fmt.Printf("%v is a cube", v.(Cube))
+	for i := 0; i < len(arr); i++ {
+		arr[i] = i
 	}
 
+	duration := time.Since(start)
+	fmt.Println(duration)
+	start = time.Now()
+	arr1 := [100000]int{}
+	for i := 0; i < 100000; i++ {
+		arr1[i] = i
+	}
+	duration = time.Since(start)
+	fmt.Println(duration)
 }
